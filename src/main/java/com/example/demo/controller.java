1bf8e6a43d450aa52295157e38d8e0db0de44771
KaushanCruze <blackout.feedback@gmail.com>
2024-12-03 06:09:20 +0530

    fixes

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -6,7 +6,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 import java.io.BufferedReader;
@@ -24,10 +23,10 @@ public class controller {
     int column=-1;
 
     @FXML
-    private VBox colNames;
+    private ChoiceBox<Map.Entry<String, Integer>> colSelect;
 
     @FXML
-    private TextField pathString;
+    private Label pathString;
 
     @FXML
     private TextArea resultView;
@@ -38,79 +37,79 @@ public class controller {
 
     @FXML
     public void initialize() {
-      
+        colSelect.setConverter(new javafx.util.StringConverter<>() {
+            @Override
+            public String toString(Map.Entry<String, Integer> object) {
+                return object==null ? "" : object.getKey();
+            }
+
+            @Override
+            public Map.Entry<String, Integer> fromString(String s) {
+                return null;
+            }
+        });
+
     }
     @FXML
     protected void startSort() {
+        int column=colSelect.getValue().getValue();
+        System.out.println(colSelect.getValue().getValue());
+        dataTable.getItems().clear();
 
-        if(column!=-1){
-            dataTable.getItems().clear();
-    
-            Thread thread = new Thread(() -> {
-                String message = "";
-                long start = System.nanoTime(); //performance check
-                ArrayList<functions.SingleDataFormat> resultShell = data.shellSort(column);
-                double shellTime = (System.nanoTime() - start) / 1e6;
-                message += "Shell sort: " + String.format("%.2f", shellTime) + " ms\n";
-               
-    
-    
-                start = System.nanoTime();
-                ArrayList<functions.SingleDataFormat> resultMerge = data.mergeSort(column);
-                double mergeTime = (System.nanoTime() - start) / 1e6;
-                message += "Merge sort: " + String.format("%.2f", mergeTime) + " ms\n";
-               
-    
-                start = System.nanoTime();
-                ArrayList<functions.SingleDataFormat> resultHeap = data.heapSort(column);
-                double heapTime = (System.nanoTime() - start) / 1e6;
-                message += "Heap sort: " + String.format("%.2f", heapTime) + " ms\n";
-             
-    
-                start = System.nanoTime();
-                ArrayList<functions.SingleDataFormat> resultQuick = data.quickSort(column);
-                double quickTime = (System.nanoTime() - start) / 1e6;
-                message += "Quick sort: " + String.format("%.2f", quickTime) + " ms\n";
-              
-    
-                start = System.nanoTime();
-                ArrayList<functions.SingleDataFormat> resultInsertion = data.insertionSort(column);
-                double insertionTime = (System.nanoTime() - start) / 1e6;
-                message += "Insertion sort: " + String.format("%.2f", insertionTime) + " ms";
-              
-                resultView.setText(message); //display
-    
-                for (int i = 0; i < resultShell.size(); i++) {
-                    ObservableList<String> row = FXCollections.observableArrayList(Arrays.asList(data.data.get(resultShell.get(i).index).value));
-                    dataTable.getItems().add(row);
-                }
-    
-                
-            });
-            thread.start();
-            
-        }
+        Thread thread = new Thread(() -> {
+            String message = "";
+            long start = System.nanoTime();
+            ArrayList<functions.SingleDataFormat> resultShell = data.shellSort(column);
+            double shellTime = (System.nanoTime() - start) / 1e6;
+            message += "Shell sort: " + String.format("%.2f", shellTime) + " ms\n";
+
+            start = System.nanoTime();
+            ArrayList<functions.SingleDataFormat> resultMerge = data.mergeSort(column);
+            double mergeTime = (System.nanoTime() - start) / 1e6;
+            message += "Merge sort: " + String.format("%.2f", mergeTime) + " ms\n";
+
+            start = System.nanoTime();
+            ArrayList<functions.SingleDataFormat> resultHeap = data.heapSort(column);
+            double heapTime = (System.nanoTime() - start) / 1e6;
+            message += "Heap sort: " + String.format("%.2f", heapTime) + " ms\n";
+
+            start = System.nanoTime();
+            ArrayList<functions.SingleDataFormat> resultQuick = data.quickSort(column);
+            double quickTime = (System.nanoTime() - start) / 1e6;
+            message += "Quick sort: " + String.format("%.2f", quickTime) + " ms\n";
+
+            start = System.nanoTime();
+            ArrayList<functions.SingleDataFormat> resultInsertion = data.quickSort(column);
+            double insertionTime = (System.nanoTime() - start) / 1e6;
+            message += "Insertion sort: " + String.format("%.2f", insertionTime) + " ms";
+
+            resultView.setText(message);
+
+            for (int i = 0; i < resultShell.size(); i++) {
+                ObservableList<String> row = FXCollections.observableArrayList(Arrays.asList(data.data.get(resultShell.get(i).index).value));
+                dataTable.getItems().add(row);
+            }
+        });
+        thread.start();
     }
-//csv file upload
+
     @FXML
     protected void handleFileSelection() {
         data=new functions();
-        colNames.getChildren().clear();
+        colSelect.getItems().clear();
         dataTable.getColumns().clear();
         dataTable.getItems().clear();
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open CSV File");
-        File selectedFile = fileChooser.showOpenDialog(colNames.getScene().getWindow());
-        if(selectedFile!=null){
-            String fileName = selectedFile.getName();
-            if(!fileName.endsWith(".csv")){
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("File not valid, Please select a csv file !");
-                alert.showAndWait();
-                return;
-            }
+        File selectedFile = fileChooser.showOpenDialog(colSelect.getScene().getWindow());
+
+        if (!selectedFile.getName().endsWith(".csv")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Please select a CSV file !");
+            alert.showAndWait();
+            return;
         }
 
         if (selectedFile != null) {
@@ -133,7 +132,6 @@ public class controller {
                 }
                 boolean firstLine = true;
                 int x=0;
-                ToggleGroup toggleGroup=new ToggleGroup();
                 while ((line = br.readLine()) != null) {
                     String[] rowValues = line.split(",");
                     data.data.add(new Data(x, rowValues));
@@ -144,15 +142,8 @@ public class controller {
                             try{
                                 Double.parseDouble(rowValues[i]);
                                 int finalI = i;
-                                RadioButton radioButton=new RadioButton();
-                                radioButton.setText(headers[finalI]);
-                                radioButton.setToggleGroup(toggleGroup);
-                                radioButton.setOnAction(event -> {
-                                    column= finalI;
-                                    System.out.println(column);
-                                });
-                                colNames.getChildren().add(radioButton);
-                            
+                                colSelect.getItems().add(new AbstractMap.SimpleEntry<>(headers[finalI], finalI));
+                                colSelect.setValue(new AbstractMap.SimpleEntry<>(headers[finalI], finalI));
                             }catch(NumberFormatException e){
                             }
                         }
